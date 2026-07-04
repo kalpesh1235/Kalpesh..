380e38d5ba19a3230932b7a70dcd1bf1d30f9fdb
kalpesh1235 <kpatil1742005@gmail.com>
2026-07-04 07:20:15 +0530

    Add files via upload

---
+++ help.js
@@ -0,0 +1,21 @@
+function Help() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Help</h2>
+
+      <h4>How to use?</h4>
+      <ol>
+        <li>Enter your personal details.</li>
+        <li>Add education, skills, and projects.</li>
+        <li>Select a resume template.</li>
+        <li>Preview your resume.</li>
+        <li>Download it as a PDF.</li>
+      </ol>
+
+      <h4>Support</h4>
+      <p>Email: support@smartresume.com</p>
+    </div>
+  );
+}
+
+export default Help;
