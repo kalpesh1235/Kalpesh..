f580aee2160065999fc2d164ea3a1f71c456f44e
kalpesh1235 <kpatil1742005@gmail.com>
2026-07-04 07:22:01 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Smart Resume Builder</h1>
+      <p>Create professional resumes for students quickly and easily.</p>
+    </div>
+  );
+}
+
+export default Home;
