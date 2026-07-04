2cf43a31fe3a9b11626417e2f9348901a6894664
kalpesh1235 <kpatil1742005@gmail.com>
2026-07-04 07:18:45 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>About</h2>
+      <p>
+        Smart Resume Builder helps students create professional resumes
+        using ready-made templates. Users can add personal details,
+        education, skills, projects, and download resumes as PDF.
+      </p>
+    </div>
+  );
+}
+
+export default About;
